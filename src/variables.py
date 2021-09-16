5785b9da9c29dfdcd6594710f0037e8ec3450efe
styled <smyvensestime@gmail.com>
2021-09-15 23:45:22 -0400

    variables that are called multiple times and some secrets

--- src/variables.py
+++ src/variables.py
@@ -1,2 +1,26 @@
+import boto3
+
 active_alarms_file = 'active_alarms.csv'
 active_alarms_dup_file = 'active_alarms-dup.csv'
+
+
+def get_secret(code_secret):
+    secret_name = "Alarm_Discord_Bot"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager',
+                            region_name=region_name)
+
+    # Get every secret stored
+    response = str(client.get_secret_value(SecretId=secret_name))
+    response = response[response.find('SecretString'):response.find('}') + 1]
+
+    # only return the secret that is asked for
+    secret_value = [i for i in range(len(response)) if response.startswith('"', i)]
+    x, y = 0, 1
+    for secrets in range(int(len(secret_value) / 2)):
+        if response[secret_value[x] + 1:secret_value[y]] == code_secret:
+            return response[secret_value[x + 2] + 1:secret_value[y + 2]]
+        x += 4; y += 4
